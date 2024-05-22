0687556efbb9eb7ae290e8b3941138b2f6c5c97d
JcesarPz <cesarpdep@gmail.com>
2024-05-22 16:16:07 -0400

    added initial code from v15

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "product_synonyms",
+    'name': "Product synomyms",
 
     'summary': "Short (1 phrase/line) summary of the module's purpose",
 
@@ -15,14 +15,14 @@ Long description of module's purpose
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '17.0.3.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['product'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
--- models/models.py
+++ models/models.py
@@ -1,19 +1,59 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api, _
+
+class ProductSynonym(models.Model):
+    _name="product.template.synonym"
+    _description="Product template synonym"
+    
+    product_id=fields.Many2one(
+        comodel_name="product.template",
+        string="Producto"
+    )
+    name=fields.Char(
+        string="Name",
+        required=True
+    )
+    
+class ProductTemplate(models.Model):
+    _inherit="product.template"
+    
+    synonym_ids=fields.One2many(
+        string="Other names",
+        comodel_name="product.template.synonym",
+        inverse_name="product_id"
+    )
+    
+    all_names=fields.Char(
+        compute="_compute_all_names"
+    )
+    
+    
+    @api.depends('name','synonym_ids')
+    def _compute_all_names(self):
+        for rec in self:
+            name_list=[] 
+            if rec.synonym_ids:           
+                for syn in rec.synonym_ids:
+                    name_list.append(syn.name)
+            name_list.append(rec.name)        
+            rec.all_names= "",name_list
+    
+    @api.model        
+    def _search_all_names(self, operator, value):
+        return ['|','|',('name',operator,value),('synonym_ids',operator,value)]
+            
+            
+    @api.model
+    def _name_search(self, name='', args=None, operator='ilike', limit=100, name_get_uid=None):
+        args=args or []
+        domain=[]
+        if name:
+            domain=[('all_names',operator,name)]
+        return self._search(domain + args, limit=limit, access_rights_uid=name_get_uid)
+            
 
 
-# class product_synonyms(models.Model):
-#     _name = 'product_synonyms.product_synonyms'
-#     _description = 'product_synonyms.product_synonyms'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
-
+    
+    
+        
